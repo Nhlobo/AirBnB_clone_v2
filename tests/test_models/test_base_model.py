779e8fd50fb59b7741b2de5d53f876970a91c797
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-20 23:48:38 +0200

    Delete tests/test_models directory

--- tests/test_models/test_base_model.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from models.base_model import BaseModel
-
-class TestBaseModel(unittest.TestCase):
-    def test_create_instance(self):
-        obj = BaseModel()
-        self.assertIsNotNone(obj)
-
-    # Add more test cases as needed
