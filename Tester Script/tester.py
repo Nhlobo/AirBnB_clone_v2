4d9dea13386fee19c5dcfe0458f18f720e39dca1
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-20 22:39:18 +0200

    Create tester.py

---
+++ Tester Script/tester.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import unittest
+from tests.test_models.test_base_model import TestBaseModelDocs, TestBaseModel
+from tests.test_models.test_amenity import TestAmenityDocs
+from tests.test_models.test_city import TestCityDocs
+from tests.test_models.test_place import TestPlaceDocs
+from tests.test_models.test_review import TestReviewDocs
+from tests.test_models.test_state import TestStateDocs
+from tests.test_models.test_user import TestUserDocs
+
+if __name__ == "__main__":
+    unittest.main()
